61e3c7c85aa9c6baa35f21c5ad2860008071e458
Miguel Angel Aguilar Garcia <migue_ag18@hotmail.com>
2019-06-08 09:22:48 -0500

    Se mejora la visualización de los datos y de las gráficas

--- prueba_caida_tension.m
+++ prueba_caida_tension.m
@@ -1,10 +1,11 @@
-clear;
+close;
 clc;
+clear;
 
 V_base = 1000;
 
 Longitud = 100;
-fp_carga = [0.9];
+fp_carga = [0.9 0.8 0.7];
 %Tabla 9. NEC/NOM-001-SEDE
 R_cable = 10.2; %Calibre 14
 X_cable = 0.240; %Calibre 14
@@ -20,10 +21,10 @@ accuracy=1e-12;
 
 maxiter=80;
 
-carga = [10000];%Carga (W)
+carga = [10000 9900 9800];%Carga (W)
 
 for mmm=1:length(fp_carga)
-fprintf('fp: %1.1f\n',fp_carga(mmm))
+fprintf('\nfp: %1.1f\n',fp_carga(mmm))
 for nnn=1:length(carga)
 % 1--slack
 % 0--load
@@ -49,7 +50,7 @@ end
 
 Ze = R_cable/1000*Longitud*fp_carga(mmm) + X_cable/1000*Longitud*sin(acos(fp_carga(mmm))); %Formula NEC/NOM-001-SEDE
 
-fprintf('n\t'), fprintf('(W)Carg\t'), fprintf('%%V(flujos)\t'), fprintf('%%V(formula)\t'), fprintf('dif\n')
+fprintf('n\t'), fprintf('(W)Carg\t'), fprintf('%%V(flujos)\t'), fprintf('%%V(fórmula)\t'), fprintf('dif\n')
 for nnn=1:length(carga)
   
 Inom(nnn,mmm) = carga(nnn)/(sqrt(3)*V_base*fp_carga(mmm));
@@ -59,16 +60,37 @@ fprintf('%f\n', V_caida_porcentaje(nnn,mmm)-caida_tension_calculada_simple(nnn,m
 
 end
 
-plot(carga,V_caida_porcentaje(:,mmm),carga,caida_tension_calculada_simple(:,mmm),carga,V_caida_porcentaje(:,mmm)-caida_tension_calculada_simple(:,mmm))
+end
+
+
+for mmm=1:length(fp_carga)
+fprintf('\nfp: %1.1f\n',fp_carga(mmm))
+fprintf('n\t'), fprintf('(W)Carg\t'), fprintf('%%V(flujos)\t'), fprintf('%%V(fórmula)\t'), fprintf('dif\n')
+for nnn=1:length(carga)
+fprintf('%g\t ', nnn), fprintf('%g\t',carga(nnn)), fprintf('%f\t', V_caida_porcentaje(nnn,mmm)), fprintf('%f\t', caida_tension_calculada_simple(nnn,mmm)),
+fprintf('%f\n', V_caida_porcentaje(nnn,mmm)-caida_tension_calculada_simple(nnn,mmm))
+end
+
+subplot(2,1,1);
+plot(carga,V_caida_porcentaje(:,mmm),'b',carga,caida_tension_calculada_simple(:,mmm),'g')
 xlabel('Carga (W)')
 ylabel('%\DeltaV')
-legend('%\DeltaV(flujos)', '%\DeltaV(formula)','%\DeltaV(flujos) - %\DeltaV(formula)')
+legend('%\DeltaV(flujos)', '%\DeltaV(fórmula)')
 str = strcat('fp: ', num2str(fp_carga(mmm)));
 text(carga(length(carga)), V_caida_porcentaje(length(carga),mmm), str)
-text(carga(length(carga)), caida_tension_calculada_simple(length(carga),mmm), str)
+text(carga(length(carga))+ (carga(1)-carga(length(carga)))/20, caida_tension_calculada_simple(length(carga),mmm)+ (caida_tension_calculada_simple(1)-caida_tension_calculada_simple(length(carga),mmm))/7, str)
 
-text(carga(length(carga)), V_caida_porcentaje(length(carga),mmm)-caida_tension_calculada_simple(length(carga),mmm), str)
 grid on
 hold on
 
+subplot(2,1,2); 
+plot(carga,(V_caida_porcentaje(:,mmm)-caida_tension_calculada_simple(:,mmm)),'r')
+xlabel('Carga (W)')
+ylabel('%\DeltaV')
+legend('(%\DeltaV(flujos) - %\DeltaV(fórmula))')
+str = strcat('fp: ', num2str(fp_carga(mmm)));
+text(carga(length(carga)), (V_caida_porcentaje(length(carga),mmm)-caida_tension_calculada_simple(length(carga),mmm)), str)
+
+grid on
+hold on
 end
